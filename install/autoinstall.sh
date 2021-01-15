a949179c8ea9c85a1d5782b77e477c8ee751b4bd
ruo-xi <314386327@qq.com>
2021-01-15 23:43:42 +0800

    add man-pages

--- install/autoinstall.sh
+++ install/autoinstall.sh
@@ -2,7 +2,7 @@ ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 hwclock --systohc
 timedatectl set-local-rtc true # 是archlinux 使用CST时间 避免因为使用UTC与windows 冲突
 
-pacman -S wpa_supplicant networkmanager ntfs-3g os-prober grub efibootmgr dhcpcd man man-db git 
+pacman -S wpa_supplicant networkmanager ntfs-3g os-prober grub efibootmgr dhcpcd man man-db man-pages git 
 
 sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
 # sed -i 's/#zh_CN.UTF-8 UTF-8/zh-CN.UTF_8 UTF-8/g' /etc/locale.gen
