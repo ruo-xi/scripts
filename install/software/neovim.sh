178ffd8873c5e8c8636e2c9ed24da3499ba5dc6c
ruo-xi <314386327@qq.com>
2021-01-21 17:15:44 +0800

    fix some problem eg: .config.ssh to .config/.ssh

--- install/software/neovim.sh
+++ install/software/neovim.sh
@@ -1,7 +1,11 @@
 yay -S neovim-nightly
 
+export https_proxy="127.0.0.1:8888"
+export http_proxy="127.0.0.1:8888"
+
 pip install neovim 
 pip2 install neovim
-
+sudo npm install -g neovim
+yay -S xclip
 npm -g install instant-markdown-d
 
