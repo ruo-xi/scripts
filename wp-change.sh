ed946599c4fc367555d4ec4b0db8005f2904b9e4
ruo-xi <314386327@qq.com>
2022-04-14 18:08:30 +0800

    update 2022-04-14

--- wp-change.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# feh --recursive --randomize --bg-scale ~/store/picture/wallpaper/wallpaper1
-
-feh --recursive --randomize --bg-scale ~/store/picture/wallpaper/*
-# feh --recursive --randomize --bg-center ~/store/picture/wallpaper/coleman.svg.png
-# feh --recursive --randomize --bg-fill ~/Pictures/wallpapers/view
